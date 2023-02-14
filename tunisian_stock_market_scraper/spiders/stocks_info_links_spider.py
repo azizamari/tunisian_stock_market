fb86ef0ae9c309bbb68c166262ee78c3c2bb9d95
azizamari <azizamari2003@gmail.com>
2023-02-14 15:05:49 +0100

    scrape link for each stock

---
+++ tunisian_stock_market_scraper/spiders/stocks_info_links_spider.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+import scrapy
+
+
+class StocksInfoLinksSpider(scrapy.Spider):
+    name = "stocks_info_links"
+
+    def start_requests(self):
+        urls = [
+            'https://www.ilboursa.com/marches/aaz',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        for link in response.css('.allf a::attr(href)').getall():
+            name=link[9:]
+            yield {
+                'name':name,
+                'link':'https://www.ilboursa.com/marches/aaz/'+link
+            }
