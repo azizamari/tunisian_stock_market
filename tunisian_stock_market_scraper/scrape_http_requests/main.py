61753f8bec1e82811ba70f7d6e427e7baf450b93
azizamari <azizamari2003@gmail.com>
2023-02-15 17:03:22 +0100

    Apply type formatting for extracted data

--- tunisian_stock_market_scraper/scrape_http_requests/main.py
+++ tunisian_stock_market_scraper/scrape_http_requests/main.py
@@ -4,12 +4,21 @@ import pandas as pd
 from all_dates import get_all_dates
 from getting_data import get_data
 
-tickers=pd.read_csv('stock_names.csv').name.values
+tickers=pd.read_csv('stock_tickers.csv').ticker.values
 dates=get_all_dates()
-for ticker in tickers[:1]:
+for ticker in tickers:
     print(f"handling ticker {ticker}")
     history=pd.DataFrame()
     for date in dates:
         res=get_data(ticker,date[0],date[1])
         history=pd.concat([res,history])
-    history.to_csv(ticker+'.csv',index=False)
+
+    history.ouverture=history.ouverture.str.replace(',','.').astype(float)
+    history.haut=history.haut.str.replace(',','.').astype(float)
+    history.cloture=history.cloture.str.replace(',','.').astype(float)
+    history.bas=history.bas.str.replace(',','.').astype(float)
+    history.date=pd.to_datetime(history.date, format="%d/%m/%Y")
+
+    history.to_csv('data/'+ticker+'.csv',index=False)
+
+# ouverture, haut, bas, cloture
